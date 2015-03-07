746dd93467c37291d217c1efd4ecfdc18680c8ac
Brian Tanabe <btanabe2@gmail.com>
2015-03-07 15:11:29 -0800

    Committing to switch computers...

--- src/main/java/com/btanabe2/mediaremote/vlc/launcher/VlcLauncher.java
+++ src/main/java/com/btanabe2/mediaremote/vlc/launcher/VlcLauncher.java
@@ -1,10 +1,8 @@
 package com.btanabe2.mediaremote.vlc.launcher;
 
 import com.btanabe2.mediaremote.vlc.commands.VlcLauncherCommand;
-import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
-import java.io.StringWriter;
 
 /**
  * Created by Brian on 3/6/15.
@@ -12,11 +10,37 @@ import java.io.StringWriter;
 public class VlcLauncher {
 
     public static String playVideoFile(VlcLauncherCommand launcherCommand) throws IOException, InterruptedException {
-        Process process = Runtime.getRuntime().exec(launcherCommand.getVlcLaunchCommand());
-        process.waitFor();
+        System.out.println(String.format("Running: [%s]", launcherCommand.getVlcLaunchCommand()));
 
-        StringWriter writer = new StringWriter();
-        IOUtils.copy(process.getInputStream(), writer);
-        return IOUtils.toString(process.getInputStream());
+//        ExecutorService executorService = Executors.newFixedThreadPool(2);
+//        Thread videoThread = new Thread(new VlcVideoThread(launcherCommand));
+//        videoThread.start();
+
+        Process process = Runtime.getRuntime().exec(launcherCommand.getVlcLaunchCommand());
+        System.out.println("Wait for time: " + process.waitFor());
+
+        System.out.println("Thread running");
+
+        return "";
+    }
+
+    private static class VlcVideoThread implements Runnable {
+        private VlcLauncherCommand launcherCommand;
+
+        public VlcVideoThread(VlcLauncherCommand launcherCommand) {
+            this.launcherCommand = launcherCommand;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Process process = Runtime.getRuntime().exec(launcherCommand.getVlcLaunchCommand());
+                System.out.println("Wait for time: " + process.waitFor());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/test/java/com/btanabe2/mediaremote/test/fixtures/PathFixtures.java
+++ src/test/java/com/btanabe2/mediaremote/test/fixtures/PathFixtures.java
@@ -5,5 +5,5 @@ package com.btanabe2.mediaremote.test.fixtures;
  */
 public class PathFixtures {
     public static final String TEST_VLC_EXECUTABLE_PATH = "/Applications/VLC.app/Contents/MacOS/VLC";
-    public static final String TEST_MOVIE_FILE_PATH = "~/Downloads/Wet\\ Hot\\ American\\ Summer.m4v";
+    public static final String TEST_MOVIE_FILE_PATH = "/Users/brian/Downloads/Love.Rosie.2014.720p.WEB-DL.DD5.1.H.264-PLAYNOW.mkv";
 }
