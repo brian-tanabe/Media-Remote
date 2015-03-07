57e64e78baa810c738e5efd59029ecfe80a11ca1
Brian Tanabe <btanabe2@gmail.com>
2015-03-06 21:47:42 -0800

    Updated test execution string.  Removed the "." which I don't need since I'm planning on using the absolute path.

--- src/test/java/com/btanabe2/mediaremote/test/unit/VlcLauncherCommandMacTests.java
+++ src/test/java/com/btanabe2/mediaremote/test/unit/VlcLauncherCommandMacTests.java
@@ -15,6 +15,6 @@ public class VlcLauncherCommandMacTests {
     @Test
     public void shouldBeAbleToFormatTheVlcLauncherCommandForOsxProperly() {
         VlcLauncherCommandMac launcher = new VlcLauncherCommandMac(TEST_VLC_EXECUTABLE_PATH);
-        assertEquals("Did not generate the correct VLC executable command", String.format(".%s -vvv %s --fullscreen", TEST_VLC_EXECUTABLE_PATH, TEST_MOVIE_FILE_PATH), launcher.getVlcLaunchCommand());
+        assertEquals("Did not generate the correct VLC executable command", String.format("%s -vvv %s --fullscreen", TEST_VLC_EXECUTABLE_PATH, TEST_MOVIE_FILE_PATH), launcher.getVlcLaunchCommand());
     }
 }
