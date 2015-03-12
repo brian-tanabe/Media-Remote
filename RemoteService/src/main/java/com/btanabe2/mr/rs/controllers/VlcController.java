798fb1839e1f1d4d4133b73aea6c0192d4d3bdd6
Brian Tanabe <btanabe2@gmail.com>
2015-03-12 00:08:36 -0700

    Stubbing out controller classes responsible for starting VLC files.

---
+++ RemoteService/src/main/java/com/btanabe2/mr/rs/controllers/VlcController.java
@@ -0,0 +1,13 @@
+package com.btanabe2.mr.rs.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Brian on 3/11/15.
+ */
+@RestController
+@RequestMapping("/media/play/")
+public class VlcController {
+
+}
---
+++ RemoteService/src/main/java/com/btanabe2/mr/rs/domain/MediaInfo.java
@@ -0,0 +1,16 @@
+package com.btanabe2.mr.rs.domain;
+
+/**
+ * Created by Brian on 3/11/15.
+ */
+public class MediaInfo {
+    private int videoId;
+
+    public MediaInfo(int videoId) {
+        this.videoId = videoId;
+    }
+
+    public int getVideoId() {
+        return videoId;
+    }
+}
