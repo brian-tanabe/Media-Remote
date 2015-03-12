833f1f54acf725423151dca71c498a55ebf15816
Brian Tanabe <btanabe2@gmail.com>
2015-03-12 00:20:43 -0700

    Committing spring config file.  Decided to make MediaInfo use the full path of the file to play.

--- RemoteService/src/main/java/com/btanabe2/mr/rs/domain/MediaInfo.java
+++ RemoteService/src/main/java/com/btanabe2/mr/rs/domain/MediaInfo.java
@@ -4,13 +4,19 @@ package com.btanabe2.mr.rs.domain;
  * Created by Brian on 3/11/15.
  */
 public class MediaInfo {
-    private int videoId;
+    private final long id;
+    private final String filePath;
 
-    public MediaInfo(int videoId) {
-        this.videoId = videoId;
+    public MediaInfo(long id, String filePath) {
+        this.id = id;
+        this.filePath = filePath;
     }
 
-    public int getVideoId() {
-        return videoId;
+    public long getId() {
+        return id;
+    }
+
+    public String getFilePath() {
+        return filePath;
     }
 }
